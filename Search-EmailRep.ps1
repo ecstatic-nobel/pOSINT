3fc686eb45d0d84cbd6a275a5c6b4e1d9d3abe47
ecstatic-nobel <ecstatic.nobel@protonmail.com>
2019-10-20 15:21:54 -0400

    Require API key and update help page.

--- Search-EmailRep.ps1
+++ Search-EmailRep.ps1
@@ -6,7 +6,7 @@
     Gather Open-Source Intelligence from EmailRep.io using PowerShell.
 
     .EXAMPLE
-    Search-EmailRep -Query bill@microsoft.com | Format-List
+    Search-EmailRep -Query bill@microsoft.com -ApiKey APIKEY | Format-List
 
     email      : bill@microsoft.com
     reputation : high
@@ -23,6 +23,8 @@ function Search-EmailRep {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
+        [string]$ApiKey,
+        [Parameter(Mandatory=$true)]
         [string]$Query
     )
     
@@ -31,7 +33,9 @@ function Search-EmailRep {
         Set-ModuleDefaults
 
         $UserAgent = "Mozilla/5.0 (Windows NT 10.0; Microsoft Windows 10.0.15063; en-US) PowerShell/6.0.0"
-        $Uri = "emailrep.io/$Query".ToLower()
+        $Uri = "https://emailrep.io/$Query".ToLower()
+        $Body = "@{Key='$ApiKey'}"
+        $ExtraRequestParams = "-Body $Body"
     }
     Process {Search-Api}
     End {Reset-SslDefaults; Write-Verbose "Complete"}
